128967570c96ca345de2bbb476ad5a545e02e98c
coulscoe <143137210+coulscoe@users.noreply.github.com>
2023-09-06 11:42:06 -0600

    created folders, and added code for the first lab

---
+++ Unit_one/Labs/Lab_one_var_oper.cs
@@ -0,0 +1,23 @@
+int myNum = 5;
+	//creates a number
+double myDoubleNum = 4.5D;
+	//this is a number that includes a decimal. including D is an option
+char myLetter = 'I';
+	//makes a varible with a single letter
+bool myBool = false;
+	//makes a true or false variable
+string myText = "this is an assignment";
+	//a variable that is more than one letter or word
+		//all of these makes that program display the above variables.
+Console.WriteLine(myNum);
+Console.WriteLine(myDoubleNum);
+Console.WriteLine(myLetter);
+Console.WriteLine(myBool);
+Console.WriteLine(myText);
+	//the section below is includes addition, multiplication, and division
+Console.WriteLine(100 + 10);
+	//+ is addition
+Console.WriteLine(10*10);
+	//* is multiplication
+Console.WriteLine(25/5);
+	// / is for division
