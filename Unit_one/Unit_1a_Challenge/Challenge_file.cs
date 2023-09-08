45fd3d5628c3e3158e9bdae18c17541dcdde3ba7
coulscoe <143137210+coulscoe@users.noreply.github.com>
2023-09-07 23:28:30 -0600

    added challenge file

---
+++ Unit_one/Unit_1a_Challenge/Challenge_file.cs
@@ -0,0 +1,23 @@
+using System;
+					
+int number = 2000;
+string aSentence = "Running code.";
+bool itsFalse = true;
+double theNumber = 10.003;
+//just setting up the values
+
+Console.WriteLine(number);
+Console.WriteLine(aSentence);
+Console.WriteLine(itsFalse);
+Console.WriteLine(theNumber);
+//writing code to display on the console
+Console.WriteLine(2000/23);
+	int x = 23;
+    int y = 3;
+    Console.WriteLine(x * y);
+//creating code displaying the code above.
+
+int r = 93;
+int z = 0;
+//add a number to replace 0
+Console.WriteLine(r-z);
